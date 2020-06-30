be7c76ea7bc08bb3773332b25ca9e0ad4f6ce87e
Thanussian Sharvananthan <thanussian@gmail.com>
2020-06-30 18:14:41 -0400

    improved file structure

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+export default function Login() {
+  return (
+    <Text> Hello, world! </Text>
+  );
+}
+//<Image style={styles.tinyLogo} source={{uri: 'https://reactnative.dev/img/tiny_logo.png',}}
